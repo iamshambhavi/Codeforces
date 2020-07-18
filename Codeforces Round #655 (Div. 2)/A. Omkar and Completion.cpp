55bab4390be24c033e97b3de8bd16dbfcc481dc4
Shambhavi Sahay <41591299+iamshambhavi@users.noreply.github.com>
2020-07-18 07:54:22 +0530

    Create A. Omkar and Completion.cpp

---
+++ Codeforces Round #655 (Div. 2)/A. Omkar and Completion.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define long long ll
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	//freopen("input.txt","r",stdin);
+	//freopen("output.txt","r",stdout);
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		for(int i=0;i<n;i++)
+		cout<<"1"<<" ";
+		cout<<endl;
+	}
+}
